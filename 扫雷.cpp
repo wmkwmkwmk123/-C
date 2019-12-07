4537c6d239eafb4e6f615ac2ee82e293864e579f
1176647591@qq.com <1176647591@qq.com>
2019-12-07 15:33:56 +0800

    扫雷

---
+++ 扫雷.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void game()
+{//建立地图 设置地雷  打印地图 
+}
+int menu()
+{printf("*****************\n");
+ printf("**欢迎来到扫雷***\n");
+ printf("**选1开始游戏***\n");
+ printf("**选2结束游戏***\n");
+ printf("****************\n");
+ int choice;
+ scanf("%d",&choice);
+ return choice; 
+}
+int main()
+{
+	while(1)
+	{
+	
+int choice=menu();
+if(choice==1)
+{game();
+}
+else if(choice==2)
+{printf("拜拜");
+return 0;
+}
+else
+{printf("您的输入有误");
+}
+}
+}
