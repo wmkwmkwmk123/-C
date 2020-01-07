36bbdde547356fa2393e3069afc326be467379f5
1176647591@qq.com <1176647591@qq.com>
2020-01-07 23:42:54 +0800

    代码

---
+++ 9×9.cpp
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+main()
+
+{int i,j,result;
+
+for (i=1;i<10;i++)
+
+{ for(j=1;j<10;j++)
+
+{
+
+result=i*j;
+
+printf("%d*%d=%-3d",i,j,result);/*-3d表示左对齐，占3位*/
+
+}
+
+printf("\n");/*每一行后换行*/
+
+}
+
+}
