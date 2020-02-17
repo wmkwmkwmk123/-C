3bcc9f70495033587378b87ce5f708d57ba4e409
1176647591@qq.com <1176647591@qq.com>
2020-02-17 22:47:14 +0800

    Signed-off-by: 1176647591@qq.com <1176647591@qq.com>

---
+++ 2.17.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int num()
+{double a=1.0;
+int b=2.0;
+double result;
+result=a/b;
+printf("%d\n",result);
+system("pause");
+return 0;
+ } 
+ int main()
+ {int p=10;
+ int *q=&p;
+ printf("%d\n",p);
+ printf("%d\n",*q);
+ printf("%p",p) ;
+ return 0;
+ }
